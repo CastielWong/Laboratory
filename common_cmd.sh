7a217b4bab606ca6c4fa16c55f55604c6c93cca1
CastielWong <castielwongsysu@gmail.com>
2021-01-26 15:45:25 +1100

    Rftr(root): Update commit-msg constraint and add an alias to uninstall all pip packages; Update Oracle SQL

--- common_cmd.sh
+++ common_cmd.sh
@@ -4,6 +4,8 @@ alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 
 alias gct_update='git commit --amend --no-edit --date "$(date +'%a %b %d %H:%M:%S %Y %z')"'
 
+alias pip_uninstall_all="pip freeze | xargs pip uninstall -y"
+
 alias dkr_psf='docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}"'
 alias dkr_if='docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.ID}}" | sort -k 1 -h'
 # remove any existed or created (failed) containers
