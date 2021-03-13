3dac89669c63d4a1be38444bd5de7e9b3e444eeb
CastielWong <castielwongsysu@gmail.com>
2021-03-14 10:26:31 +1100

    Build(airflow): Complete the sample for the usage of task group

--- Airflow/Compose/v_distributed/volume/dags/demo_parallel_task_group.py
+++ Airflow/Compose/v_distributed/volume/dags/demo_parallel_task_group.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 
 from airflow.models import DAG
 from airflow.operators.bash import BashOperator
+from airflow.utils.task_group import TaskGroup
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -19,26 +20,49 @@ default_args = {
     "start_date": datetime(2021, 1, 1),
 }
 
-dag = DAG(
+with DAG(
     dag_id="demo_pipeline_parallel_task_group",
     description="This is a DAG for simple demo",
     catchup=False,
     max_active_runs=1,
     schedule_interval=timedelta(days=1),
     default_args=default_args,
-)
+) as dag:
+    task_1 = BashOperator(
+        dag=dag, task_id="task_1", bash_command="sleep 2; echo This is Task 1"
+    )
 
-task_1 = BashOperator(
-    dag=dag, task_id="task_1", bash_command="sleep 2; echo This is Task 1"
-)
-task_2 = BashOperator(
-    dag=dag, task_id="task_2", bash_command="sleep 2; echo This is Task 2"
-)
-task_3 = BashOperator(
-    dag=dag, task_id="task_3", bash_command="sleep 2; echo This is Task 3"
-)
-task_4 = BashOperator(
-    dag=dag, task_id="task_4", bash_command="sleep 2; echo This is Task 4"
-)
+    with TaskGroup("processing_tasks") as processing:
+        task_2 = BashOperator(
+            dag=dag, task_id="task_2", bash_command="sleep 2; echo This is Task 2"
+        )
 
-task_1 >> [task_2, task_3] >> task_4
+        # embed task group deeper
+        with TaskGroup("spark_tasks") as spark_tasks:
+            task_3_1 = BashOperator(
+                dag=dag,
+                task_id="task_3_1",
+                bash_command="sleep 2; echo This is Task 3_1 for Spark",
+            )
+            task_3_2 = BashOperator(
+                dag=dag,
+                task_id="task_3_2",
+                bash_command="sleep 2; echo This is Task 3_2 for Spark",
+            )
+        with TaskGroup("flink_tasks") as flink_tasks:
+            task_3_1 = BashOperator(
+                dag=dag,
+                task_id="task_3_1",
+                bash_command="sleep 2; echo This is Task 3_1 for Flink",
+            )
+            task_3_2 = BashOperator(
+                dag=dag,
+                task_id="task_3_2",
+                bash_command="sleep 2; echo This is Task 3_2 for Flink",
+            )
+
+    task_4 = BashOperator(
+        dag=dag, task_id="task_4", bash_command="sleep 2; echo This is Task 4"
+    )
+
+    task_1 >> processing >> task_4
